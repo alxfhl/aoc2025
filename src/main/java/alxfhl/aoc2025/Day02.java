c6d473824c8a2c6fcb732c654088e6ba3fc90c68
Alexander Fuhl <110429902+alxfhl@users.noreply.github.com>
2025-12-05 08:43:17 +0100

    Day02

--- src/main/java/alxfhl/aoc2025/Day02.java
+++ src/main/java/alxfhl/aoc2025/Day02.java
@@ -1,22 +1,104 @@
 package alxfhl.aoc2025;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day02 {
 
+    static long[] factors = {
+            0L, 11L, 101L, 1_001L, 10_001L, 100_001L, 1_000_001L, 10_000_001L, 100_000_001L, 1_000_000_001L
+    };
+
+    record LongRange(long min, long max) {
+    }
+
 
     public static long solve(String s) {
-        final List<String> lines = Arrays.stream(s.split("\n")).filter(l -> !l.isBlank()).toList();
-
+        final List<LongRange> ranges = parseRanges(s);
         long result = 0;
+        for (LongRange range : ranges) {
+            long start = getNext(range.min - 1);
+            long end = getPrev(range.max + 1);
+            for (long x = start; x <= end; x = getNext(x)) {
+                result += x;
+            }
+        }
         return result;
     }
 
-    public static long solve2(String s) {
-        final List<String> lines = Arrays.stream(s.split("\n")).filter(l -> !l.isBlank()).toList();
+    public static long getNext(long min) {
+        String s = Long.toString(min);
+        int len = s.length();
+        if (len % 2 == 0) {
+            long factor = factors[len / 2];
+            long candidate = Long.parseLong(s.substring(0, len / 2));
 
+            if (candidate * factor > min) {
+                return candidate * factor;
+            } else {
+                return (candidate + 1) * factor;
+            }
+        }
+        long factor = factors[len / 2 + 1];
+        return factor / 10 * factor;
+    }
+
+    public static long getPrev(long max) {
+        String s = Long.toString(max);
+        int len = s.length();
+        if (len % 2 == 0) {
+            long factor = factors[len / 2];
+            long candidate = Long.parseLong(s.substring(0, len / 2));
+
+            if (candidate * factor < max) {
+                return candidate * factor;
+            } else {
+                return (candidate - 1) * factor;
+            }
+        }
+        long factor = factors[len / 2];
+        return (factor - 2) * factor;
+    }
+
+    public static long solve2(String s) {
+        final List<LongRange> ranges = parseRanges(s);
         long result = 0;
+        for (LongRange range : ranges) {
+            for (long x = range.min; x <= range.max; x++) {
+                if (isInvalid(x)) {
+                    result += x;
+                }
+            }
+        }
+        return result;
+    }
+
+    private static boolean isInvalid(long x) {
+        String s = Long.toString(x);
+        int length = s.length();
+        for (int len = 1; len <= length / 2; len++) {
+            if ((length % len) != 0) {
+                continue;
+            }
+            int repeat = length / len;
+            if (s.equals(s.substring(0, len).repeat(repeat))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static List<LongRange> parseRanges(String s) {
+        List<LongRange> result = new ArrayList<>();
+        for (String range : s.split("[\n, ]+")) {
+            int minus = range.indexOf('-');
+            if (minus == -1) {
+                continue;
+            }
+            long min = Long.parseLong(range.substring(0, minus));
+            long max = Long.parseLong(range.substring(minus + 1));
+            result.add(new LongRange(min, max));
+        }
         return result;
     }
 }
--- src/test/java/alxfhl/aoc2025/Day02Test.java
+++ src/test/java/alxfhl/aoc2025/Day02Test.java
@@ -6,28 +6,52 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Day02Test {
 
     public static final String EXAMPLE1 = """
-            
+            11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+            1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+            824824821-824824827,2121212118-2121212124
             """;
 
     @Test
     void part1() throws IOException, URISyntaxException {
-        assertThat(Day02.solve(EXAMPLE1)).isEqualTo(3);
+        assertThat(Day02.solve(EXAMPLE1)).isEqualTo(1_227_775_554L);
 
-        Path path = Path.of(Day02.class.getResource("Day02.txt").toURI());
-        assertThat(Day02.solve(Files.readString(path))).isEqualTo(42);
+        Path path = Path.of(Objects.requireNonNull(Day02.class.getResource("Day02.txt")).toURI());
+        assertThat(Day02.solve(Files.readString(path))).isEqualTo(38_310_256_125L);
     }
 
     @Test
     void part2() throws IOException, URISyntaxException {
-        assertThat(Day02.solve2(EXAMPLE1)).isEqualTo(6);
+        assertThat(Day02.solve2(EXAMPLE1)).isEqualTo(4_174_379_265L);
 
-        Path path = Path.of(Day02.class.getResource("Day02.txt").toURI());
-        assertThat(Day02.solve2(Files.readString(path))).isEqualTo(42);
+        Path path = Path.of(Objects.requireNonNull(Day02.class.getResource("Day02.txt")).toURI());
+        assertThat(Day02.solve2(Files.readString(path))).isEqualTo(58_961_152_806L);
+    }
+
+    @Test
+    void testNext() {
+        assertThat(Day02.getNext(10)).isEqualTo(11);
+        assertThat(Day02.getNext(11)).isEqualTo(22);
+        assertThat(Day02.getNext(95)).isEqualTo(99);
+        assertThat(Day02.getNext(998)).isEqualTo(1010);
+        assertThat(Day02.getNext(1188511880)).isEqualTo(1188511885);
+        assertThat(Day02.getNext(123)).isEqualTo(1010);
+        assertThat(Day02.getNext(5446793)).isEqualTo(10001000);
+    }
+
+    @Test
+    void testPrev() {
+        assertThat(Day02.getPrev(23)).isEqualTo(22);
+        assertThat(Day02.getPrev(115)).isEqualTo(99);
+        assertThat(Day02.getPrev(1012)).isEqualTo(1010);
+        assertThat(Day02.getPrev(1188511890)).isEqualTo(1188511885);
+        assertThat(Day02.getPrev(5446793)).isEqualTo(999999); // 10001
+
     }
 }
