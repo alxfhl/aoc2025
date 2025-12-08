d8018a601c86e44e788120c79f02c71d8d672670
Alexander Fuhl <110429902+alxfhl@users.noreply.github.com>
2025-12-08 08:32:33 +0100

    Day08

--- src/main/java/alxfhl/aoc2025/Day08.java
+++ src/main/java/alxfhl/aoc2025/Day08.java
@@ -1,14 +1,118 @@
 package alxfhl.aoc2025;
 
+import lombok.RequiredArgsConstructor;
+
+import java.util.*;
+
 public class Day08 {
 
-    public static long solve(String s) {
-        long result = 0;
-        return result;
+    @RequiredArgsConstructor
+    static class Node {
+        final long x;
+        final long y;
+        final long z;
+        Graph graph = new Graph(this);
+
+        static Node parse(String s) {
+            String[] split = s.trim().split("[^0-9\\-]+");
+            if (split.length != 3) {
+                throw new IllegalArgumentException("Invalid input for a Node: '" + s + "'");
+            }
+            return new Node(Long.parseLong(split[0]), Long.parseLong(split[1]), Long.parseLong(split[2]));
+        }
+
+        public long distSqr(Node node2) {
+            long dx = x - node2.x;
+            long dy = y - node2.y;
+            long dz = z - node2.z;
+            return dx * dx + dy * dy + dz * dz;
+        }
+    }
+
+    static class Edge {
+        final Node node1;
+        final Node node2;
+        final double distance;
+
+        Edge(Node node1, Node node2) {
+            this.node1 = node1;
+            this.node2 = node2;
+            this.distance = Math.sqrt(node1.distSqr(node2));
+        }
+    }
+
+    static class Graph {
+        final Set<Node> nodes = new HashSet<>();
+
+        Graph(Node... nodes) {
+            Collections.addAll(this.nodes, nodes);
+        }
+    }
+
+    public static long solve(String s, int combine, int multiply) {
+        final List<Node> nodes = parseNodes(s);
+        final List<Edge> edges = precalculateEdges(nodes);
+        for (int i = 0; i < combine; i++) {
+            Edge edge = edges.get(i);
+            Graph graph1 = edge.node1.graph;
+            Graph graph2 = edge.node2.graph;
+            if (graph1 != graph2) {
+                // merge both graphs
+                graph1.nodes.addAll(graph2.nodes);
+                for (Node node : graph2.nodes) {
+                    node.graph = graph1;
+                }
+            }
+        }
+
+        final Set<Graph> graphs = new TreeSet<>(Comparator.comparingInt(g -> -g.nodes.size()));
+        nodes.forEach(n -> graphs.add(n.graph));
+
+        return graphs.stream().limit(multiply).mapToLong(g -> g.nodes.size()).reduce(1, (a, b) -> a * b);
     }
 
     public static long solve2(String s) {
-        long result = 0;
-        return result;
+        final List<Node> nodes = parseNodes(s);
+        final List<Edge> edges = precalculateEdges(nodes);
+        final List<Graph> graphs = new ArrayList<>();
+        nodes.forEach(n -> graphs.add(n.graph));
+
+        for (Edge edge : edges) {
+            Graph graph1 = edge.node1.graph;
+            Graph graph2 = edge.node2.graph;
+            if (graph1 != graph2) {
+                // merge both graphs
+                graph1.nodes.addAll(graph2.nodes);
+                for (Node node : graph2.nodes) {
+                    node.graph = graph1;
+                }
+                graphs.remove(graph2);
+                if (graphs.size() == 1) {
+                    return edge.node1.x * edge.node2.x;
+                }
+            }
+        }
+        throw new IllegalStateException("No solution found");
+    }
+
+    private static List<Edge> precalculateEdges(List<Node> nodes) {
+        final List<Edge> edges = new ArrayList<>();
+        for (int n1 = 0; n1 < nodes.size(); n1++) {
+            for (int n2 = n1 + 1; n2 < nodes.size(); n2++) {
+                edges.add(new Edge(nodes.get(n1), nodes.get(n2)));
+            }
+        }
+        edges.sort(Comparator.comparing(e -> e.distance));
+        return edges;
+    }
+
+    private static List<Node> parseNodes(String s) {
+        final List<Node> nodes = new ArrayList<>();
+        for (String line : s.split("\n")) {
+            if (!line.isBlank()) {
+                nodes.add(Node.parse(line));
+            }
+        }
+        return nodes;
     }
 }
--- src/test/java/alxfhl/aoc2025/Day08Test.java
+++ src/test/java/alxfhl/aoc2025/Day08Test.java
@@ -12,21 +12,41 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class Day08Test {
 
     public static final String EXAMPLE1 = """
+            162,817,812
+            57,618,57
+            906,360,560
+            592,479,940
+            352,342,300
+            466,668,158
+            542,29,236
+            431,825,988
+            739,650,466
+            52,470,668
+            216,146,977
+            819,987,18
+            117,168,530
+            805,96,715
+            346,949,466
+            970,615,88
+            941,993,340
+            862,61,35
+            984,92,344
+            425,690,689
             """;
 
     @Test
     void part1() throws IOException, URISyntaxException {
-        assertThat(Day08.solve(EXAMPLE1)).isEqualTo(1_227_775_554L);
+        assertThat(Day08.solve(EXAMPLE1, 10, 3)).isEqualTo(40);
 
         Path path = Path.of(Day08.class.getResource("Day08.txt").toURI());
-        assertThat(Day08.solve(Files.readString(path))).isEqualTo(38_310_256_125L);
+        assertThat(Day08.solve(Files.readString(path), 1000, 3)).isEqualTo(83_520);
     }
 
     @Test
     void part2() throws IOException, URISyntaxException {
-        assertThat(Day08.solve2(EXAMPLE1)).isEqualTo(4_174_379_265L);
+        assertThat(Day08.solve2(EXAMPLE1)).isEqualTo(25272);
 
         Path path = Path.of(Day08.class.getResource("Day08.txt").toURI());
-        assertThat(Day08.solve2(Files.readString(path))).isEqualTo(58_961_152_806L);
+        assertThat(Day08.solve2(Files.readString(path))).isEqualTo(1_131_823_407);
     }
 }
