91d8b1b9d074c1fede02a4367d4ee81a796c2cfd
Alexander Fuhl <110429902+alxfhl@users.noreply.github.com>
2025-12-11 09:05:48 +0100

    Day11

--- src/main/java/alxfhl/aoc2025/Day11.java
+++ src/main/java/alxfhl/aoc2025/Day11.java
@@ -1,14 +1,60 @@
 package alxfhl.aoc2025;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.function.Predicate.not;
+
 public class Day11 {
+
+    @Data
+    @EqualsAndHashCode(of = "name")
+    static class Node {
+        final String name;
+        List<Node> connections = new ArrayList<>();
+        List<Node> incoming = new ArrayList<>();
+
+        public String toString() {
+            return name + "(in: " + incoming.stream().map(Node::getName).collect(Collectors.joining(","))
+                    + " / out: " + connections.stream().map(Node::getName).collect(Collectors.joining(",")) + ")";
+        }
+    }
 
     public static long solve(String s) {
         long result = 0;
         return result;
     }
 
-    public static long solve2(String s) {
-        long result = 0;
-        return result;
+    private static Map<String, Node> getAllNodes(String s) {
+        List<String> lines = Arrays.stream(s.split("\n")).map(String::trim).filter(not(String::isEmpty)).toList();
+        Map<String, Node> map = new HashMap<>();
+        lines.stream()
+                .flatMap(line -> Stream.of(line.split("[: ]+")))
+                .distinct()
+                .forEach(name -> map.put(name, new Node(name)));
+        for (String line : lines) {
+            String name = line.substring(0, line.indexOf(":"));
+            String[] connections = line.split(": ")[1].split(" ");
+            Node node = map.get(name);
+            for (String connection : connections) {
+                node.connections.add(map.get(connection));
+            }
+        }
+
+        // we want to propagate a node's value only when we handled all incoming connections, so we need to keep track of those
+        Map<Node, List<Node>> incomingConnections = new HashMap<>();
+        for (Node node : map.values()) {
+            for (Node connection : node.connections) {
+                incomingConnections.computeIfAbsent(connection, _ -> new ArrayList<>()).add(node);
+            }
+        }
+        for (var entry : incomingConnections.entrySet()) {
+            entry.getKey().incoming.addAll(entry.getValue());
+        }
+        return map;
     }
 }
--- src/test/java/alxfhl/aoc2025/Day11Test.java
+++ src/test/java/alxfhl/aoc2025/Day11Test.java
@@ -12,21 +12,46 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class Day11Test {
 
     public static final String EXAMPLE1 = """
+            aaa: you hhh
+            you: bbb ccc
+            bbb: ddd eee
+            ccc: ddd eee fff
+            ddd: ggg
+            eee: out
+            fff: out
+            ggg: out
+            hhh: ccc fff iii
+            iii: out
+            """;
+    public static final String EXAMPLE2 = """
+            svr: aaa bbb
+            aaa: fft
+            fft: ccc
+            bbb: tty
+            tty: ccc
+            ccc: ddd eee
+            ddd: hub
+            hub: fff
+            eee: dac
+            dac: fff
+            fff: ggg hhh
+            ggg: out
+            hhh: out
             """;
 
     @Test
     void part1() throws IOException, URISyntaxException {
-        assertThat(Day11.solve(EXAMPLE1)).isEqualTo(1_227_775_554L);
+        assertThat(Day11.solve(EXAMPLE1)).isEqualTo(5);
 
         Path path = Path.of(Day11.class.getResource("Day11.txt").toURI());
-        assertThat(Day11.solve(Files.readString(path))).isEqualTo(38_310_256_125L);
+        assertThat(Day11.solve(Files.readString(path))).isEqualTo(413);
     }
 
     @Test
     void part2() throws IOException, URISyntaxException {
-        assertThat(Day11.solve2(EXAMPLE1)).isEqualTo(4_174_379_265L);
+        assertThat(Day11.solve2(EXAMPLE2)).isEqualTo(2);
 
         Path path = Path.of(Day11.class.getResource("Day11.txt").toURI());
-        assertThat(Day11.solve2(Files.readString(path))).isEqualTo(58_961_152_806L);
+        assertThat(Day11.solve2(Files.readString(path))).isEqualTo(525_518_050_323_600L);
     }
 }
